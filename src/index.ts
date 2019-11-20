caa323870dc418556218658bdfe9dc23495b10b8
jeemyeong <jeemyeong@gmail.com>
2019-11-20 12:05:49 +0900

    Export Options from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { createInterceptMiddleware } from "./redux-intercept";
-export { createConfirmMiddleware } from "./redux-confirm";
-export { createWaitMiddleware } from "./redux-wait";
-export { createEnricherMiddleware } from "./redux-enricher"
-export { createRecordMiddleware } from "./redux-record"
-export { createThrottleMiddleware } from "./redux-throttle"
+export { createInterceptMiddleware, InterceptOptions } from "./redux-intercept";
+export { createConfirmMiddleware, ConfirmOptions } from "./redux-confirm";
+export { createWaitMiddleware, WaitOptions } from "./redux-wait";
+export { createEnricherMiddleware, EnricherOptions } from "./redux-enricher"
+export { createRecordMiddleware, RecordOptions } from "./redux-record"
+export { createThrottleMiddleware, ThrottleOptions } from "./redux-throttle"
